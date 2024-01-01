045df224533980e639076cfc57f32bcd2b6a725b
Zane <z.pearton@accenture.com>
2024-01-01 16:12:31 +1100

    Update main.go

--- main.go
+++ main.go
@@ -2,15 +2,42 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World! This is a basic web server!")
-}
-
 func main() {
-    http.HandleFunc("/", handler) // Set the handler function for the root path
-    fmt.Println("Server is running on http://localhost:8080") // Print a message indicating where the server is running
-    http.ListenAndServe(":8080", nil) // Start the server on port 8080
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "Hello, World! This is a cool web server!")
+    })
+
+    server := &http.Server{
+        Addr:         ":8080",
+        Handler:      mux,
+        ReadTimeout:  5 * time.Second,
+        WriteTimeout: 10 * time.Second,
+        IdleTimeout:  15 * time.Second,
+    }
+
+    go func() {
+        fmt.Println("Server is running on http://localhost:8080")
+        if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+            log.Fatalf("ListenAndServe(): %v", err)
+        }
+    }()
+
+    // Graceful Shutdown
+    quit := make(chan os.Signal, 1)
+    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+    <-quit
+    if err := server.Shutdown(nil); err != nil {
+        log.Fatal("Server forced to shutdown:", err)
+    }
+
+    log.Println("Server exiting")
 }
